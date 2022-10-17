4161024fb4b48bc1429b5438808952e95971d900
Ashley Meyer <ashcalebmeyer@gmail.com>
2022-10-17 21:08:16 +0200

    Completed exercise for reverseString.js

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,15 @@
-const reverseString = function() {
+const reverseString = function(word) {
+
+//let reversedWordArray = [];
+let reversedWord = "";
+
+for (i = word.length; i > 0; i--)
+{
+    //reversedWordArray[word.length-i] = word.charAt(i-1);  
+    reversedWord = reversedWord.concat(word.charAt(i-1))
+}
+
+return reversedWord;
 
 };
 
