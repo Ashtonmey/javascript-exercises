ebe3afaa473f8780f1ab36b76cf65744adc6bd3b
Ashley Meyer <ashcalebmeyer@gmail.com>
2022-10-23 18:42:53 +0200

    Completed sumAll.js

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,27 @@
-const sumAll = function() {
+const sumAll = function(int1, int2) {
+
+if (int1 < 0 || int2 < 0 || Number.isInteger(int1) == false || Number.isInteger(int2) == false)
+{
+    return "ERROR";
+}
+
+let sum = 0;
+
+let smallInt = int1; //assume first integer is smaller
+let bigInt = int2; //assume second integer is bigger
+
+if (int1 > int2) // test the inverse and swap them around if true
+{
+    smallInt = int2;
+    bigInt = int1;
+}
+
+for (i =  smallInt; i <= bigInt; i++)
+{
+    sum = sum + i;
+}
+
+return sum;
 
 };
 
